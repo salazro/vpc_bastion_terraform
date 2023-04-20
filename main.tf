c517dfc64881cfc7d6caf5c1ddd9d9d3988934e8
Rommel Salazar <salazro69@gmail.com>
2023-04-20 14:46:38 -0500

    Update main.tf
    Signed-off-by: Rommel Salazar <salazro69@gmail.com>

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-## Configure the AWS Provider
-provider "aws" {
-  region     = "us-east-1"
-  access_key = local.db_access.access_key
-  secret_key = local.db_access.secret_key
-}
-
 data "aws_secretsmanager_secret" "secrets" {
   arn = "arn:aws:secretsmanager:us-east-1:612016699982:secret:github_actions-GuEf2m"
 }
@@ -16,6 +9,15 @@ data "aws_secretsmanager_secret_version" "current" {
 
 locals {
   db_access = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)
+}
+
+
+
+## Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = local.db_access.access_key
+  secret_key = local.db_access.secret_key
 }
 
 resource "aws_vpc" "vpc1" {
