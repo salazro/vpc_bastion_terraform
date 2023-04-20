b08c189637c34a7f50c783584e84d86b1c1a6877
Rommel Salazar <salazro69@gmail.com>
2023-04-20 14:24:06 -0500

    Delete secrets.tf
    Signed-off-by: Rommel Salazar <salazro69@gmail.com>

--- secrets.tf
+++
@@ -1,7 +0,0 @@
-data "aws_secretsmanager_secret_version" "credential" {
-  secret_id = github_actions
-}
-
-locals {
-  db_creds = jsondecode(data.aws_secretsmanager_secret_version.credential.secret_string)
-}
