d68dfc0fc8037d4178d9562460c6b1be36cef13a
Rommel Salazar <salazro69@gmail.com>
2023-04-25 13:48:03 -0500

    correcting access error

--- provider.tf
+++ provider.tf
@@ -1,15 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
-  shared_config_files      = ["C:/Users/rdsal/.aws/config"]
-  shared_credentials_files = ["C:/Users/rdsal/.aws/credentials"]
-  profile                  = "terraform"
+  region= "us-east-1"
+  profile = "terraform"
 
 }
